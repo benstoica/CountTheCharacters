06632d42735ba5e62a4b3e43b1166848ebbe7313
Ben Stoica <bennystoica@gmail.com>
2020-05-24 20:10:49 -0400

    Add files via upload

---
+++ CountTheCharacters/VariablesAndStrings/CountTheCharacters/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace CountTheCharacters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Please enter the word you would like to know the length of.");
+                string userWord = Console.ReadLine();
+                while (string.IsNullOrEmpty(userWord))
+                {
+                    Console.WriteLine("You have to enter a word! Please enter the word you would like to know the length of.");
+                    userWord = Console.ReadLine();
+                }
+                int wordLength = userWord.Length;
+                Console.WriteLine($"{userWord} has {wordLength} characters.");
+
+                Console.WriteLine("Would you like to know the length of another word? (Y/N)");
+                string checkAnother = Console.ReadLine().ToUpper();
+                if (checkAnother == "Y")
+                {
+                    continue;
+                }
+
+                else if (checkAnother == "N")
+                {
+                    Console.WriteLine("Thank you! Have a great day!");
+                    return;
+                }
+            }
+        }
+    }
+}
